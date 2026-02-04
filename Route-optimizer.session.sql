789dc4c193e601e0add51a74e9a4c66046dcd915
miskadelux <mihakosma1@gmail.com>
2026-02-04 21:48:08 +0100

    the database is done

---
+++ Route-optimizer.session.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Routes (
+    district_Number INT PRIMARY KEY,
+    name Varchar(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE Stops (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    lat DECIMAL(9, 6) NOT NULL,
+    lon DECIMAL(9, 6) NOT NULL,
+    mailbox_count INT NOT NULL,
+    district_Number INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_stops_routes FOREIGN KEY (district_Number) REFERENCES Routes(district_Number) ON DELETE CASCADE
+);
+INSERT INTO Routes (district_Number, name)
+VALUES (720, 'SDR 0720 – Fridlevstad');
+INSERT INTO Stops (district_Number, lat, lon, mailbox_count)
+VALUES (720, 56.184233, 15.621984, 2);
+SELECT *
+FROM Routes;
+SELECT *
+FROM Stops;
