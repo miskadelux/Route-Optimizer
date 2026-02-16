e309e4f0962f8e081ed0b85e7d4a8d26338f4e15
miskadelux <mihakosma1@gmail.com>
2026-02-16 14:21:21 +0100

    Update Route-optimizer.session.sql

--- Route-optimizer.session.sql
+++ Route-optimizer.session.sql
@@ -1,3 +1,9 @@
+CREATE TABLE Mailboxes (
+    ID int PRIMARY KEY AUTO_INCREMENT,
+    stop_id int NOT NULL,
+    mailbox_status varchar(50) NOT NULL,
+    CONSTRAINT fk_mailboxes_stops FOREIGN KEY (stop_id) REFERENCES Stops(id) ON DELETE CASCADE
+);
 CREATE TABLE Routes (
     district_Number INT PRIMARY KEY,
     name Varchar(255) NOT NULL,
@@ -7,8 +13,6 @@ CREATE TABLE Stops (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     lat DECIMAL(9, 6) NOT NULL,
     lon DECIMAL(9, 6) NOT NULL,
-    mailbox_count INT NOT NULL,
     district_Number INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_stops_routes FOREIGN KEY (district_Number) REFERENCES Routes(district_Number) ON DELETE CASCADE
-);
