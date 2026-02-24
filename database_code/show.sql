e69c135781bf8123d114b1ed1b5d05de2858cc2c
miskadelux <mihakosma1@gmail.com>
2026-02-24 21:54:05 +0100

    Create show.sql

---
+++ database_code/show.sql
@@ -0,0 +1,16 @@
+/*SELECT *
+ from routes;
+ SELECT *
+ from stops;
+ 
+ -- drop table routes;
+ 
+ 
+ UPDATE mailboxes
+ SET mailbox_status = "nej tack"
+ where addresses = 'friedlevstad 334';
+ 
+ SELECT *
+ from mailboxes
+ 
+ */
